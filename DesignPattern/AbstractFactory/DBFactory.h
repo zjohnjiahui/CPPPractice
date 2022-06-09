1d88b3e0dffd480bbfad223866558505bb827b7b
zhujiahui <zhujiahui_zjohn@foxmail.com>
2022-06-09 20:35:22 +0800

    Update DBFactory.h

--- DesignPattern/AbstractFactory/DBFactory.h
+++ DesignPattern/AbstractFactory/DBFactory.h
@@ -2,16 +2,19 @@
 
 class IDBConnection {
 public:
+  virtual ~IDBConnection() { }
   virtual void connect() = 0;
 };
 
 class IDBCommand {
 public:
+  virtual ~IDBCommand() { }
   virtual void command() = 0;
 };
 
 class IDataReader {
 public:
+  virtual ~IDataReader() { }
   virtual void read() = 0;
 };
 
@@ -21,5 +24,4 @@ public:
   virtual IDBConnection* CreateDBConnection() = 0;
   virtual IDBCommand* CreateDBCommand() = 0;
   virtual IDataReader* CreateDataReader() = 0;
-
 };
