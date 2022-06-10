6508b389ecb81d83264039f246d73a00fb392756
zhujiahui <zhujiahui_zjohn@foxmail.com>
2022-06-10 11:20:27 +0800

    Update main.cpp

--- DesignPattern/Singleton/main.cpp
+++ DesignPattern/Singleton/main.cpp
@@ -5,10 +5,6 @@ int main()
   Singleton* instance_1 = Singleton::Instance();
   Singleton* instance_2 = Singleton::Instance();
   assert(instance_1 == instance_1);
-   
-  DerivedSingleton* a = DerivedSingleton::Instance();
-  DerivedSingleton* b = DerivedSingleton::Instance();
-  assert(a == b);
-
+ 
   return 0;
 }
